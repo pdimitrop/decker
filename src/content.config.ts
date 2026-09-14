ac6c62853c7c6197bf6dadcbd4d07cd046b4ab1c
pdimitrop <67342384+pdimitrop@users.noreply.github.com>
2026-09-14 23:18:58 +0300

    Modify blog categories in content configuration
    Updated blog categories to include coding, tutorials, career, tools-workflow, and notes.

--- src/content.config.ts
+++ src/content.config.ts
@@ -26,7 +26,7 @@ const blog = defineCollection({
         slug: z.string().max(50),
         title: z.string().max(50),
         publishedDate: z.date(),
-        category: z.enum(["systems", "ai", "productivity"]), // change and add blog categories here
+        category: z.enum(["coding", "tutorials", "career", "tools-workflow", "notes"]), // change and add blog categories here
         readingTime: z.number().optional(),
         isDraft: z.boolean()
     })
